18cd582e8b1055b7c4ce2a7396f397eca2b8403e
BoTech-Development <fteetz@outlook.de>
2026-04-21 11:52:02 +0200

    Added BoTech.VersionPolicy documentation and improved the docs page (giscus and header)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,42 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeGalaxy from 'starlight-theme-galaxy'
-import starlightSiteGraph from 'starlight-site-graph'
+import starlightGiscus from "starlight-giscus";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			plugins: [starlightThemeGalaxy(), starlightSiteGraph()],
-			title: 'My Docs',
+			plugins: [starlightThemeGalaxy(), starlightGiscus({
+				repo: 'BoTech-Development/BoTech.Dev.Docs-Discussions',
+				repoId: 'R_kgDOSIcKfw',
+				category: 'Q&A',
+				categoryId: 'DIC_kwDOSIcKf84C7VwQ'
+			})],
+			title: 'docs.botech.dev',
 			logo: {
-
-				src: './src/assets/BoTech-Logo-Complete.svg',
+				src: './src/assets/BoTechLogoCompleteWithSlogan.svg',
 				replacesTitle: true,
-
 			},
+			//customCss: ['./src/styles/logo.css'],
 			editLink: {
 				baseUrl: 'https://github.com/BoTech-Development/BoTech.Dev.Docs/edit/master/'
 			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/BoTech-Development/' }],
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/BoTech-Development/'
+				},
+				{
+					icon: 'email',
+					label: 'Email',
+					href: "mailto:support@botech.dev"
+				}],
+			components: {
+				SiteTitle: './src/components/overrides/CustomHeader.astro',
+			},
+			customCss: ['./src/styles/logo.css'],
 		}),
 	],
 });
